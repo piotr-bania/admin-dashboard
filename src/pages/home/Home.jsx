014f63300f6cb54e47aaec132bfa78703e1d1c92
Piotr Bania <piotr.bania@europe.com>
2022-05-13 16:25:24 +0100

    init

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,9 @@ import './home.scss'
 const Home = () => {
     return (
         <div className='home'>
-            Home.
+            <h1 className='title'>
+                Home
+            </h1>
         </div>
     )
 }
