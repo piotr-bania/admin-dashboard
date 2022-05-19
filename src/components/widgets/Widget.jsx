8d5fc2d7fc0f6a5e3587803b2377f7fa2e684ff1
Piotr Bania <piotr.bania@europe.com>
2022-05-19 11:40:49 +0100

    init

--- src/components/widgets/Widget.jsx
+++ src/components/widgets/Widget.jsx
@@ -1,10 +1,18 @@
 import './widget.scss'
+import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined'
 
 const widget = () => {
     return (
         <div className='widget'>
-            <div className="left">left</div>
-            <div className="right">right</div>
+            <div className="left">
+                <span className='title'>USERS</span>
+                <span className='counter'>21450</span>
+                <span className='link'>See all users</span>
+            </div>
+            <div className="right">
+                <ArrowDropUpOutlinedIcon className='icon'/>
+                <div className="percentage">20%</div>
+            </div>
         </div>
     )
 }
